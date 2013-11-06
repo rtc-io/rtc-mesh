da5c5c54014e1ddfdb545133c033966f84dc392d
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-07 08:29:25 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+'use strict';
+
+/**
+  # rtc-smartpeer
+
+  An experimental interface for working with RTCPeerConnection with the
+  assistance of [state.js](http://statejs.org/).
+
+**/
+
+module.exports = function(pc) {
+
+};
