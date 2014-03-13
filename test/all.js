9146586dd825ea8ec0857cbd12f3b0495c4574c6
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-13 16:08:05 +1100

    Removing tests as these need to be rewritten

--- test/all.js
+++
@@ -1,8 +0,0 @@
-// require('../').use('http://rtc.io/switchboard/');
-// require('cog/logger').enable('*');
-
-require('./peers-tracking');
-require('./update');
-require('./sync');
-require('./dataline');
-require('./datastreams');
