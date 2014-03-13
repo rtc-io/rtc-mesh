b105c86a4bb7464eec3036df10bfc7204f7d637f
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-13 16:16:26 +1100

    Added CRDT extension example

---
+++ examples/crdt.js
@@ -0,0 +1,18 @@
+var quickconnect = require('rtc-quickconnect');
+var mesh = require('../');
+var Doc = require('crdt').Doc;
+var uuid = require('uuid');
+
+// initialise the connection
+var qc = quickconnect('http://rtc.io/switchboard', {
+  room: 'meshdemo-simple'
+});
+
+// create the model
+var model = mesh(qc, { model: new Doc() });
+
+model.on('add', function(row) {
+  console.log('new row created: ', row);
+});
+
+model.add({ id: uuid.v4(), name: 'Fred' });
