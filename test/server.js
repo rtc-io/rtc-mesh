2217d960a020d42ed6acfc0b5e4c9a231f58af2e
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-30 17:37:05 +1100

    Convert server to a broth compatible server

--- test/server.js
+++ test/server.js
@@ -1,6 +1,8 @@
 var http = require('http');
-var server = http.createServer();
-var port = process.env.ZUUL_PORT || process.env.PORT;
-var switchboard = require('rtc-switchboard')(server, { servelib: true });
 
-server.listen(parseInt(port, 10) || 3000);
+module.exports = function() {
+  var server = http.createServer();
+  var switchboard = require('rtc-switchboard')(server, { servelib: true });
+
+  return server;
+};
